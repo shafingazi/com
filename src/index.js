d7a631915015469cc2cfe5424206cf6ee5a1913f
shafingazi <34550119+shafingazi@users.noreply.github.com>
2021-11-10 03:23:06 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import * as THREE from "three";
 class App extends Component {
   componentDidMount() {
-    // === THREE.JS CODE START ===
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    // document.body.appendChild( renderer.domElement );
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    this.mount.appendChild( renderer.domElement );
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     var cube = new THREE.Mesh( geometry, material );
@@ -21,11 +22,10 @@ class App extends Component {
       renderer.render( scene, camera );
     };
     animate();
-    // === THREE.JS EXAMPLE CODE END ===
   }
   render() {
     return (
-      <div />
+      <div ref={ref => (this.mount = ref)} />
     )
   }
 }
